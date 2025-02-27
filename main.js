b9400e9a3228bd7dedbf19c5eb2e21e9f868b2d5
Marlene <marlenescerbak@gmail.com>
2025-02-27 13:06:46 -0800

    implementing dynamic pet area

--- main.js
+++ main.js
@@ -10,15 +10,31 @@ async function temperatureFinder() {
 
 temperatureFinder()
 
+function createAgeText(birthYear) {
+  const currYear = new Date().getFullYear()
+  const age = currYear - birthYear
+  if (age == 0) {
+    return "Less than a year old"
+  } else {
+    return age + " year" + (age > 1 ? "s" : "") + " old"
+  }
+}
+
 async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
+
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-description").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
     wrapper.appendChild(clone)
   })
   document.querySelector(".list-of-pets").appendChild(wrapper)
 }
+
 
 petsArea()
